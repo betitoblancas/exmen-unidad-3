a9710bfd8811089e787df3e1dc56daefaa561e2a
betitoblancas <betitoo.x86@gmail.com>
2021-11-10 10:53:33 -0600

    version 1

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', power: 'hola'},
+  { id: 12, name: 'Narco' , power: 'hola'},
+  { id: 13, name: 'Bombasto', power: 'hola'},
+  { id: 14, name: 'Celeritas', power: 'hola'},
+  { id: 15, name: 'Magneta', power: 'hola' },
+  { id: 16, name: 'RubberMan', power: 'hola' },
+  { id: 17, name: 'Dynama', power: 'hola' },
+  { id: 18, name: 'Dr IQ', power: 'hola' },
+  { id: 19, name: 'Magma', power: 'hola' },
+  { id: 20, name: 'Tornado', power: 'hola' }
+];
